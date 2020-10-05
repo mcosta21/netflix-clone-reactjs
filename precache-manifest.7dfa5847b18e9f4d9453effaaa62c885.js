cca524e27e6df8a7c9096364858432fd7be40e1e
Marcio Costa <marcioc424@gmail.com>
2020-10-05 14:29:30 -0300

    Updates

---
+++ precache-manifest.7dfa5847b18e9f4d9453effaaa62c885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefe461cbbf1098374ed09bc9187e97d",
+    "url": "/netflix-clone-reactjs/index.html"
+  },
+  {
+    "revision": "b737c3ce916885a9a9b3",
+    "url": "/netflix-clone-reactjs/static/css/main.6cc5fdb9.chunk.css"
+  },
+  {
+    "revision": "38c39aa81b8035c7c851",
+    "url": "/netflix-clone-reactjs/static/js/2.31481c0e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netflix-clone-reactjs/static/js/2.31481c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b737c3ce916885a9a9b3",
+    "url": "/netflix-clone-reactjs/static/js/main.bb0b54ba.chunk.js"
+  },
+  {
+    "revision": "072d3d686435aba9883d",
+    "url": "/netflix-clone-reactjs/static/js/runtime-main.25994bbc.js"
+  }
+]);
