f8a4f4c22b1c6feebb15ea0d0f90ac2e73afa326
Marcio Costa <marcioc424@gmail.com>
2020-10-08 14:19:10 -0300

    Updates

---
+++ precache-manifest.23948b86d00180269a1f2ec7c16b689b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0655655e5f3b248885c2ae1607a1e8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b7f7445684b4acde2e1",
+    "url": "/static/css/main.33dda6dc.chunk.css"
+  },
+  {
+    "revision": "61b18a9061daebe60b58",
+    "url": "/static/js/2.fa2a86ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fa2a86ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7f7445684b4acde2e1",
+    "url": "/static/js/main.3a12e8b4.chunk.js"
+  },
+  {
+    "revision": "af6de86f0029f6ff6116",
+    "url": "/static/js/runtime-main.7229c6dc.js"
+  },
+  {
+    "revision": "f42807c11b0bd8a82d141e99232e8a1d",
+    "url": "/static/media/icon-amazon.f42807c1.png"
+  },
+  {
+    "revision": "262e5be1ba67422687dc70b101d0291a",
+    "url": "/static/media/icon-netflix.262e5be1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix-clone-reactjs/precache-manifest.584cdb5e5c90e3efb5893838d3a3334f.js"
+  "/precache-manifest.23948b86d00180269a1f2ec7c16b689b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix-clone-reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
